bee6746b6bd0e79a42f5151dd7ecc02585d3b7ac
Lucas Souza <lucasp.sdev@gmail.com>
2022-01-09 13:56:31 -0300

    feat: finish hero component

---
+++ components/Hero/Hero.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hero">
+    <img src="../../assets/images/movie-hero.jpg" alt="Hero banner image" />
+    <div class="text-container">
+      <div class="text">
+        <span class="mini-heading">Now Stremaing</span>
+        <h1><span class="bolder">Now</span> Streaming</h1>
+        <a href="#movie-grid" class="button">View Movies</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'HeroComponent',
+}
+</script>
+
+<style src="./Hero.scss" lang="scss" scoped />
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="home">
-
+    <Hero />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import Hero from '~/components/Hero/Hero.vue'
 
 export default Vue.extend({
-  name: 'IndexPage',
+  name: "IndexPage",
+  components: { Hero }
 })
 </script>
